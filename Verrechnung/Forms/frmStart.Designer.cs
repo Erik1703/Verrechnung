9ef9de0b8bb94d90e67df071ca49c774f96f0785
Erik Melikyan <Erik.Melikyan@s.hakmistelbach.ac.at>
2021-04-13 17:14:29 +0200

    Projektdateien hinzufügen.

---
+++ Verrechnung/Forms/frmStart.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Verrechnung
+{
+    partial class frmStart
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWarenkorb = new System.Windows.Forms.Button();
+            this.btnArtikel = new System.Windows.Forms.Button();
+            this.btnKunde = new System.Windows.Forms.Button();
+            this.btnRechnung = new System.Windows.Forms.Button();
+            this.btnUmsatzsteuer = new System.Windows.Forms.Button();
+            this.btnStatistik = new System.Windows.Forms.Button();
+            this.btnAlleSchlissen = new System.Windows.Forms.Button();
+            this.btnProgrammSchlissen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWarenkorb
+            // 
+            this.btnWarenkorb.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWarenkorb.Location = new System.Drawing.Point(54, 28);
+            this.btnWarenkorb.Name = "btnWarenkorb";
+            this.btnWarenkorb.Size = new System.Drawing.Size(139, 38);
+            this.btnWarenkorb.TabIndex = 0;
+            this.btnWarenkorb.Text = "Warenkorb";
+            this.btnWarenkorb.UseVisualStyleBackColor = true;
+            // 
+            // btnArtikel
+            // 
+            this.btnArtikel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArtikel.Location = new System.Drawing.Point(54, 75);
+            this.btnArtikel.Name = "btnArtikel";
+            this.btnArtikel.Size = new System.Drawing.Size(139, 38);
+            this.btnArtikel.TabIndex = 1;
+            this.btnArtikel.Text = "Artikel";
+            this.btnArtikel.UseVisualStyleBackColor = true;
+            // 
+            // btnKunde
+            // 
+            this.btnKunde.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKunde.Location = new System.Drawing.Point(54, 122);
+            this.btnKunde.Name = "btnKunde";
+            this.btnKunde.Size = new System.Drawing.Size(139, 38);
+            this.btnKunde.TabIndex = 2;
+            this.btnKunde.Text = "Kunde";
+            this.btnKunde.UseVisualStyleBackColor = true;
+            // 
+            // btnRechnung
+            // 
+            this.btnRechnung.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRechnung.Location = new System.Drawing.Point(54, 169);
+            this.btnRechnung.Name = "btnRechnung";
+            this.btnRechnung.Size = new System.Drawing.Size(139, 38);
+            this.btnRechnung.TabIndex = 3;
+            this.btnRechnung.Text = "Rechnung";
+            this.btnRechnung.UseVisualStyleBackColor = true;
+            // 
+            // btnUmsatzsteuer
+            // 
+            this.btnUmsatzsteuer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUmsatzsteuer.Location = new System.Drawing.Point(54, 216);
+            this.btnUmsatzsteuer.Name = "btnUmsatzsteuer";
+            this.btnUmsatzsteuer.Size = new System.Drawing.Size(139, 38);
+            this.btnUmsatzsteuer.TabIndex = 4;
+            this.btnUmsatzsteuer.Text = "Umsatzsteuer";
+            this.btnUmsatzsteuer.UseVisualStyleBackColor = true;
+            // 
+            // btnStatistik
+            // 
+            this.btnStatistik.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStatistik.Location = new System.Drawing.Point(54, 263);
+            this.btnStatistik.Name = "btnStatistik";
+            this.btnStatistik.Size = new System.Drawing.Size(139, 38);
+            this.btnStatistik.TabIndex = 5;
+            this.btnStatistik.Text = "Statistik";
+            this.btnStatistik.UseVisualStyleBackColor = true;
+            // 
+            // btnAlleSchlissen
+            // 
+            this.btnAlleSchlissen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlleSchlissen.Location = new System.Drawing.Point(54, 310);
+            this.btnAlleSchlissen.Name = "btnAlleSchlissen";
+            this.btnAlleSchlissen.Size = new System.Drawing.Size(139, 38);
+            this.btnAlleSchlissen.TabIndex = 6;
+            this.btnAlleSchlissen.Text = "Alle Schließen";
+            this.btnAlleSchlissen.UseVisualStyleBackColor = true;
+            // 
+            // btnProgrammSchlissen
+            // 
+            this.btnProgrammSchlissen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProgrammSchlissen.Location = new System.Drawing.Point(54, 357);
+            this.btnProgrammSchlissen.Name = "btnProgrammSchlissen";
+            this.btnProgrammSchlissen.Size = new System.Drawing.Size(139, 50);
+            this.btnProgrammSchlissen.TabIndex = 7;
+            this.btnProgrammSchlissen.Text = "Programm Schließen";
+            this.btnProgrammSchlissen.UseVisualStyleBackColor = true;
+            // 
+            // frmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 443);
+            this.Controls.Add(this.btnProgrammSchlissen);
+            this.Controls.Add(this.btnAlleSchlissen);
+            this.Controls.Add(this.btnStatistik);
+            this.Controls.Add(this.btnUmsatzsteuer);
+            this.Controls.Add(this.btnRechnung);
+            this.Controls.Add(this.btnKunde);
+            this.Controls.Add(this.btnArtikel);
+            this.Controls.Add(this.btnWarenkorb);
+            this.Name = "frmStart";
+            this.Text = "Verrechnung";
+            this.Load += new System.EventHandler(this.frmStart_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWarenkorb;
+        private System.Windows.Forms.Button btnArtikel;
+        private System.Windows.Forms.Button btnKunde;
+        private System.Windows.Forms.Button btnRechnung;
+        private System.Windows.Forms.Button btnUmsatzsteuer;
+        private System.Windows.Forms.Button btnStatistik;
+        private System.Windows.Forms.Button btnAlleSchlissen;
+        private System.Windows.Forms.Button btnProgrammSchlissen;
+    }
+}
+
